0729ad3f320ee9760455409ce3ed0aa101d100f0
LeonardoAlves002 <166024333+LeonardoAlves002@users.noreply.github.com>
2024-05-06 09:36:49 -0300

    Add files via upload

---
+++ treinando_logica.py
@@ -0,0 +1,64 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+
+# Resultado ...
+
+# entrada = input('Digite um numero: ')
+
+# if entrada.isdigit():
+#   entrada_int = int(entrada)
+#  par_impar = entrada_int %2 == 0
+# par_impar_texto = 'impar'
+
+
+#   if par_impar:
+#   par_impar_texto = 'par'
+
+    #print(f'O numero {entrada_int} é {par_impar_texto}')
+# else:
+    #print('Voce nao digitou um numero inteiro')
+
+
+
+
+
+"""Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
+descrito, exiba a saudação apropriada. Ex.
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23."""
+
+#horario = int(input('Que horas são? '))
+
+#if horario <= 11:
+    #print('Bom dia')
+#elif horario <= 17:
+   # print('Boa tarde')
+#else:
+    #print('Boa noite')
+
+"""
+Faça um programa que peça o primeiro nome do usuário.
+
+Se o nome tiver 4 letras ou menos escreva "Seu nome é curto";
+
+Se tiver entre 5 e 6 letras, escreva "_Seu nome é norma_l";
+
+Maior que 6 escreva "Seu nome é muito grande".
+"""
+
+#nome = input('Digite seu nome: ')
+#tamanho_nome = len(nome)
+
+
+#if tamanho_nome > 1:
+    #if tamanho_nome <= 4:
+        #print('Seu nome é curto')
+    #elif tamanho_nome >= 5 and tamanho_nome <= 6:
+        #print('Seu nome é normal')
+    #elif tamanho_nome > 6:
+        #print('Seu nome é muito grande')
+#else:
+    #print('Digite mais de uma letra')
